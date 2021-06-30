cc42a7367dd62c3dbd9c4a758be35e2276b78bd7
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2021-06-30 14:26:41 +0530

    Update approve.py

--- approve.py
+++ approve.py
@@ -15,5 +15,5 @@ CHARSET = "UTF-8"
 BODY_TEXT = ("\r\n"
             )
 # The HTML body of the email.
-<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-              <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
+"""<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+              <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
