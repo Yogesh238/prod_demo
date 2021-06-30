fd3d0452f81663f227a6e1ec7ae15dc6862d4e37
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2021-06-30 14:22:45 +0530

    Create approve.py

---
+++ approve.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.exceptions import ClientError
+from sys import argv
+import requests
+
+ENV = argv[1]
+url = argv[2]
+
+
+RECIPIENT = "sachin.saini@cloverbaytechnologies.com"
+SENDER = "yogesh.p@cloverbaytechnologies.com"
+AWS_REGION = "us-east-1"
+CHARSET = "UTF-8"
+
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = ("\r\n"
+            )
+# The HTML body of the email.
+<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+              <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
